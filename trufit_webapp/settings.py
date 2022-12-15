b1a4f03bf3b1e84adb2af274ec7595aaec1aa315
Aditya-aot <aditya.aot.11@gmail.com>
2022-12-15 11:21:28 +0530

    hosting trufit webapp

--- trufit_webapp/settings.py
+++ trufit_webapp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-h$pywtyd@40y=0n*u*5+i0uda67o=0b)s*pac&ax0wub8jbjqf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
